ab35b22877ae180c09ff5d88f246309d9ec2516c
rikaplay <richikcat51@gmail.com>
2026-07-23 22:11:26 +0300

    Switch to PREFER_PROJECT to allow Node.js repo

--- android apps/settings.gradle.kts
+++ android apps/settings.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
